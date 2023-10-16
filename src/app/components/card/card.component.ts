933135c18180f063caabfb16c16cf41237275df0
Raul Dionisio <rauldionisiosh@gmail.com>
2023-10-16 20:01:34 -0300

    Tornando card Dinâmico e criando service

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
+  name: string = "Bulbasaur"
+  attributesTypes :string[] = ['FIRE', 'ROCK']
   constructor() { }
 
   ngOnInit(): void {
