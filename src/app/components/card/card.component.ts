3d329ea8372e751c079c5c1b99f2dba1ac26d4ea
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2023-10-17 16:33:52 -0300

    Buscando pokemon dinâmicamente e mostrando no card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -6,11 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  name: string = "Bulbasaur"
+
+  pokemon : PokemonData = {
+    id: 0,
+    name: "",
+    sprites: {
+      front_default: ""
+    },
+    types: []
+  }
   attributesTypes :string[] = ['FIRE', 'ROCK']
-  constructor() { }
+  constructor(private service : PokemonService) { }
 
   ngOnInit(): void {
+    this.service.getPokemon("charizard").subscribe(
+      {
+        next: (res) => {
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+        }, // deu certo
+        error: (err) => console.log(err) // deu erro
+      }
+    )
   }
 
 }
